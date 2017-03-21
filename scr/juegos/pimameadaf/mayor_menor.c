dc198cab4e795aa41383339fb6736b81a8360a57
Matias Figueredo <matias68@gmail.com>
2017-03-21 20:52:15 -0300

    Juego Par-Impar, Mayor-Menor, Adentro- Afuera terminado

--- scr/juegos/pimameadaf/mayor_menor.c
+++ scr/juegos/pimameadaf/mayor_menor.c
@@ -3,75 +3,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
 
-	int opt=0, num1=0, num2=0, num3=0, n=5, i;
+	int num1=0, num2=0, num3=0, n=5, i, opt=0;
+	char datoIngresado[]="";
 	time_t t;
 
 	printf( "\nEste es el juego del Par-Impar, Mayor-Menor, Adentro-Afuera.\n\n");
 	printf( "\t BIENVENIDO\n\n");
-	
+
 	/*----------------Aca empieza Par Impar-----------------------*/
-	printf( " Elegi si el numero es Par(0) o Impar(1):\n\n");
 
-	scanf("%d",&opt);
-	while (opt != 0 && opt != 1){
-		printf("\nIngresá un valor valido, 0 para par o 1 para impar\n\n");
-		scanf("%d",&opt);
+	while (opt != 1 && opt != 2){
+		printf( " Elegi si el numero es Impar(1) o Par(2):\n\n");
+		scanf("%s",datoIngresado);
+		opt=atoi(datoIngresado);
 	}
 
-	srand((unsigned) time(&t));
-	num1=rand()%14;
-	while (num1==0){
-		num1=rand()%14;
-	}
 
-	printf( "\tnumero que salió = %d\n\n", num1);	
+	srand((unsigned) time(&t)); //creamos la semilla para el random
 
-	if ( num1%2==0 && opt%2==0){ 
-		printf("\tEl numero salido %d es par\n", num1); 
+	/*Buscamos un munero entre 1 y 13*/
+	num1=(rand()%13)+1;
+
+	printf( "\tnumero que salió = %d\n\n", num1);
+
+	/*Comparamos el modulo del numero con lo que se eligió */
+	if ( num1%2==0 && opt==2){
+		printf("\tEl numero salido %d es par y ganaste!\n", num1);
 	}
-	else if ( num1%2==1 && opt%2==1){ 
-		printf("\tEl numero salido %d es impar\n", num1); 
-	}else if ( num1%2==0 && opt%2==1){ 
-		printf("\tNo le pegaste, era par\n"); 
+	else if ( num1%2==1 && opt==1){
+		printf("\tEl numero salido %d es impar y ganaste!\n", num1);
+	}else if ( num1%2==0 && opt==1){
+		printf("\tNo le pegaste, era par\n");
 		return(0);
-	}else if ( num1%2==1 && opt%2==0){ 
-		printf("\tNo le pegaste, era impar\n"); 
+	}else if ( num1%2==1 && opt%2==2){
+		printf("\tNo le pegaste, era impar\n");
 		return(0);
 	}
 	/*-------------------------------------------------------------*/
 
 	/*----------------Aca empieza Mayor Menor----------------------*/
 
-	num2=rand()%14;
-	while (num2==0){
-		num2=rand()%14;
+	opt=0;
+	/*Buscamos un munero entre 1 y 13*/
+	num2=rand()%13+1;
+	while (num1==num2){
+		num2=rand()%13+1;
 	}
 
-	printf( "\n Elegi si el numero es Mayor(1) o Menor(0):\n\n");
-
-	scanf("%d",&opt);
-
-	while (opt != 0 && opt != 1){
-		printf("\nIngresá un valor valido, 1 para mayor o 0 para menor\n\n");
-		scanf("%d",&opt);
+	/*Preguntamos si el siguiente numero es mayor o menor*/
+	while (opt != 1 && opt != 2){
+		printf( "\n Elegi si el numero es Menor(1) o Mayor(2):\n\n");
+		scanf("%s",datoIngresado);
+		opt=atoi(datoIngresado);
 	}
 
-	printf( "\n\tnumero que salió = %d\n\n", num2);
+	printf( "\n\tNumero que salió = %d\n\n", num2);
 
-	if (num2 < num1 && opt==0){ 
-		printf("\tEl numero salido %d es menor que el anterior %d, ELEGISTE BIEN \n", num2, num1); 
+	/*Evaluamos si el numero que salió es mayor o menor y si se eligió bien*/
+	if (num2 < num1 && opt==1){
+		printf("\tEl numero que salio es %d es menor que el anterior %d, ELEGISTE BIEN \n", num2, num1);
 	}
-	else if (num2 > num1 && opt==1){ 
-		printf("\tEl numero salido %d es mayor al anterior %d, ELEGISTE BIEN \n", num2, num1); 
-	}else if (num2 == num1){
-		printf("\tEl numero salido %d y el numero anterior %d son iguales, que mala suerte tenes\n", num2 ,num1); 
-	}else if (num2 < num1 && opt==1){
+	else if (num2 > num1 && opt==2){
+		printf("\tEl numero que salio es %d es mayor al anterior %d, ELEGISTE BIEN \n", num2, num1);
+	}
+	else if (num2 == num1){
+		printf("\tEl numero que salio es %d y el numero anterior %d, son iguales, que mala suerte tenes\n", num2 ,num1);
+	}
+	else if (num2 < num1 && opt==2){
 		printf("\tNo le pegaste, era menor\n");
 		return(0);
-	}else if (num2 < num1 && opt==0){
+	}
+	else if (num2 > num1 && opt==1){
 		printf("\tNo le pegaste, era mayor\n");
 		return(0);
 	}
@@ -80,46 +86,66 @@ int main(int argc, char *argv[]){
 
 	/*---------------Aca empieza Adentro Afuera--------------------*/
 
-	num3=rand()%14;
-	while (num3==0){
-		num3=rand()%14;
+	opt=0;
+	/*Buscamos un munero entre 1 y 13*/
+	num3=rand()%13+1;
+	while (num1==num3){
+		num3=rand()%13+1;
+	}
+	while (num2==num3){
+		num3=rand()%13+1;
 	}
 
 	if (num2<num1) printf( "\n\tSe formó el intervalo [%d,%d] \n\n", num2, num1);
 	if (num2>num1) printf( "\n\tSe formó el intervalo [%d,%d] \n\n", num1, num2);
 
-	printf( "\n Elegi si el numero es Adentro(0) o Afuera(1):\n\n");
-
-	scanf("%d",&opt);
-
-	while (opt != 0 && opt != 1){
-		printf("\nIngresá un valor valido, 0 para menor o 1 para mayor\n\n");
-		scanf("%d",&opt);
+	while (opt != 1 && opt != 2){
+		printf( "\n Elegi si el numero es Adentro(1) o Afuera(2):\n\n");
+		scanf("%s",datoIngresado);
+		opt=atoi(datoIngresado);
 	}
 
 	printf( "\n\tEl numero que salió = %d\n\n", num3);
 
-	//intervalo con el 2do numero menor
-	if (num2<num1){
-		//el num3 entra en el intevalo
-		if (num2 < num3 && num1 >num3){ 
-			printf("\tEl numero salido %d entra en el intervalo [%d,%d] \n", num3, num2, num1); 
+	/*Elige adentro*/
+	if (opt==1){
+		//intervalo con el 1er numero menor
+		if (num1<num2){
+			if (num1 < num3 && num2 > num3){
+					printf("\tEl numero que salio es %d entra en el intervalo [%d,%d] y GANASTE\n", num3, num2, num1);
+				} else{
+					 printf("\tEl numero que salio es %d no entra en el intervalo [%d,%d] y PERDISTE\n", num3, num2, num1);
+				}
 		}
-		//el num3 no entra en el intervalo
-		else if (num1 < num3 && num2 >num3){ 
-			printf("\tEl numero salido %d no entra en el intervalo [%d,%d] \n", num3, num1, num2); 
+		//intervalo con el 2do numero menor
+		if (num2<num1){
+			//el num3 entra en el intevalo
+			if (num2 < num3 && num1 > num3){
+					printf("\tEl numero que salio es %d entra en el intervalo [%d,%d] y GANASTE\n", num3, num2, num1);
+				} else{
+					 printf("\tEl numero que salio es %d no entra en el intervalo [%d,%d] y PERDISTE\n", num3, num2, num1);
+				}
 		}
 	}
 
-	//intervalo con el 1er numero menor
-	if (num1<num2){
-		//el num3 entra en el intevalo
-		if (num1 < num3 && num2 >num3){ 
-			printf("\tEl numero salido %d entra en el intervalo [%d,%d] \n", num3, num1, num2); 
+	/*Elige afuera*/
+	if (opt==2){
+		//intervalo con el 1er numero menor
+		if (num1<num2){
+			if (num1 > num3 || num2 < num3){
+					printf("\tEl numero que salio es %d no entra en el intervalo [%d,%d] y GANASTE\n", num3, num2, num1);
+				} else{
+					 printf("\tEl numero que salio es %d entra en el intervalo [%d,%d] y PERDISTE\n", num3, num2, num1);
+				}
 		}
-		//el num3 no entra en el intervalo
-		else if (num2 < num3 && num1 >num3){ 
-			printf("\tEl numero salido %d no entra en el intervalo [%d,%d] \n", num3, num2, num1); 
+		//intervalo con el 2do numero menor
+		if (num2<num1){
+			//el num3 entra en el intevalo
+			if (num2 < num3 || num1 > num3){
+					printf("\tEl numero que salio es %d no entra en el intervalo [%d,%d] y GANASTE\n", num3, num2, num1);
+				} else{
+					 printf("\tEl numero que salio es %d entra en el intervalo [%d,%d] y PERDISTE\n", num3, num2, num1);
+				}
 		}
 	}
 
