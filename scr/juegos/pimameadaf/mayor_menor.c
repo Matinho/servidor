8fd45eb44c4e7f1fb52000d056f435de8da65b28
Matias Figueredo <matias68@gmail.com>
2017-03-21 21:31:50 -0300

    Arreglado problema en el Par Impar

--- scr/juegos/pimameadaf/mayor_menor.c
+++ scr/juegos/pimameadaf/mayor_menor.c
@@ -31,15 +31,21 @@ int main(int argc, char *argv[]){
 	printf( "\tnumero que salió = %d\n\n", num1);
 
 	/*Comparamos el modulo del numero con lo que se eligió */
+	/*Si el numero es par y si elegimos que sea par*/
 	if ( num1%2==0 && opt==2){
 		printf("\tEl numero salido %d es par y ganaste!\n", num1);
 	}
-	else if ( num1%2==1 && opt==1){
-		printf("\tEl numero salido %d es impar y ganaste!\n", num1);
-	}else if ( num1%2==0 && opt==1){
+	/*Si el numero es par y si elegimos que sea impar*/
+	else if ( num1%2==0 && opt==1){
 		printf("\tNo le pegaste, era par\n");
 		return(0);
-	}else if ( num1%2==1 && opt%2==2){
+	}
+	/*Si el numero es impar y si elegimos que sea impar*/
+	else if ( num1%2==1 && opt==1){
+		printf("\tEl numero salido %d es impar y ganaste!\n", num1);
+	}
+	/*Si el numero es impar y si elegimos que sea par*/
+	else if ( num1%2==1 && opt==2){
 		printf("\tNo le pegaste, era impar\n");
 		return(0);
 	}
